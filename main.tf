1adf95b3a3b2e792c0d33c07d6ad7944583fa85d
Jason Wiker <jason.wiker@gmail.com>
2021-09-20 16:13:44 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,13 +128,13 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     target_origin_id = "s3-cloudfront"
     
-    forwarded_values {
-      query_string = false
+#     forwarded_values {
+#       query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
     cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
     origin_request_policy_id = "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf"
     viewer_protocol_policy = "redirect-to-https"
